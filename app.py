bd20423d9b91cdd5d6687407fa2218a1149a1c7f
Pranav-Ajay <pranavajay005@gmail.com>
2026-01-28 19:16:26 +0530

    Update app.py
    Signed-off-by: Pranav-Ajay <pranavajay005@gmail.com>

--- app.py
+++ app.py
@@ -1,21 +1,54 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import mysql.connector
 
 app = Flask(__name__)
+CORS(app)
 
-db = mysql.connector.connect(
-    host="localhost",
-    port=3306,
-    user="root",
-    password="root123",
-    database="users"
-)
+def get_db():
+    return mysql.connector.connect(
+        host="localhost",
+        port=3306,
+        user="root",
+        password="root123",
+        database="users"
+    )
 
 @app.route("/users", methods=["GET"])
 def get_users():
+    db = get_db()
     cursor = db.cursor(dictionary=True)
-    cursor.execute("SELECT uid, post,password FROM users ORDER BY post")
+    cursor.execute("SELECT uid, post, password FROM users ORDER BY post")
     data = cursor.fetchall()
+    cursor.close()
+    db.close()
     return jsonify(data)
+
+@app.route("/login", methods=["POST"])
+def login():
+    data = request.get_json()
+
+    uid = data.get("uid")
+    password = data.get("password")
+
+    db = get_db()
+    cursor = db.cursor(dictionary=True)
+
+    cursor.execute(
+        "SELECT * FROM users WHERE uid=%s AND password=%s",
+        (uid, password)
+    )
+    user = cursor.fetchone()
+
+    cursor.close()
+    db.close()
+
+    if user:
+        return jsonify({"status": "success"})
+    else:
+        return jsonify({"status": "fail"})
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
 
