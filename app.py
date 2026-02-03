9c117528e80d68b4809edaa285b22e94347cacec
Pranav-Ajay <pranavajay005@gmail.com>
2026-02-03 17:31:32 +0530

    Update app.py
    Signed-off-by: Pranav-Ajay <pranavajay005@gmail.com>

--- app.py
+++ app.py
@@ -24,49 +24,6 @@ def save_fir():
     return jsonify({"status": "saved"})
 
 
-def get_db():
-    return mysql.connector.connect(
-        host="localhost",
-        port=3306,
-        user="root",
-        password="root123",
-        database="users"
-    )
-
-@app.route("/users", methods=["GET"])
-def get_users():
-    db = get_db()
-    cursor = db.cursor(dictionary=True)
-    cursor.execute("SELECT uid, post, password FROM users ORDER BY post")
-    data = cursor.fetchall()
-    cursor.close()
-    db.close()
-    return jsonify(data)
-
-@app.route("/login", methods=["POST"])
-def login():
-    data = request.get_json()
-
-    uid = data.get("uid")
-    password = data.get("password")
-
-    db = get_db()
-    cursor = db.cursor(dictionary=True)
-
-    cursor.execute(
-        "SELECT * FROM users WHERE uid=%s AND password=%s",
-        (uid, password)
-    )
-    user = cursor.fetchone()
-
-    cursor.close()
-    db.close()
-
-    if user:
-        return jsonify({"status": "success"})
-    else:
-        return jsonify({"status": "fail"})
-
 if __name__ == "__main__":
     app.run(debug=True)
 
