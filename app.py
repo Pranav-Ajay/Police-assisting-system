e60f561b9f6ac0b62169d22ecb38ce4fd98d07fd
Pranav-Ajay <pranavajay005@gmail.com>
2026-02-03 17:19:34 +0530

    Update app.py
    Signed-off-by: Pranav-Ajay <pranavajay005@gmail.com>

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import mysql.connector
+import pandas as pd
+import os
+
+@app.route("/save_fir", methods=["POST"])
+def save_fir():
+    data = request.json
+
+    file_path = "fir_dataset.csv"
+
+    df = pd.DataFrame([data])
+
+    if os.path.exists(file_path):
+        df.to_csv(file_path, mode='a', header=False, index=False)
+    else:
+        df.to_csv(file_path, mode='w', header=True, index=False)
+
+    return jsonify({"status": "saved"})
 
 app = Flask(__name__)
 CORS(app)
