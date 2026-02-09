062ab8ef866375d16645f0c9c55bcdd7090429a0
Pranav-Ajay <pranavajay005@gmail.com>
2026-02-09 19:06:51 +0530

    Create db.js
    Signed-off-by: Pranav-Ajay <pranavajay005@gmail.com>

---
+++ db.js
@@ -0,0 +1,12 @@
+const { Client } = require("pg");
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
+
+client.connect()
+  .then(() => console.log("Database connected"))
+  .catch(err => console.error("DB error:", err));
+
+module.exports = client;
