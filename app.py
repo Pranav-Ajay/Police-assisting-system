218652b8e330e63cf68bf627c48f0ca154a21a67
Pranav-Ajay <pranavajay005@gmail.com>
2026-02-09 19:03:36 +0530

    Update app.py
    Signed-off-by: Pranav-Ajay <pranavajay005@gmail.com>

--- app.py
+++ app.py
@@ -1,39 +1,52 @@
-from flask import Flask, request, jsonify
-from flask import render_template
+from flask import Flask, request, jsonify, render_template, g
 from flask_cors import CORS
-import mysql.connector
+import os
+import psycopg2
+import psycopg2.extras
 
 app = Flask(__name__)
 CORS(app)
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+
 def get_db():
-    return mysql.connector.connect(
-        host="localhost",
-        port=3306,
-        user="root",
-        password="0007",
-        database="miniproject"
-    )
+    if "db" not in g:
+        g.db = psycopg2.connect(DATABASE_URL, sslmode="require")
+    return g.db
+
+
+@app.teardown_appcontext
+def close_db(error):
+    db = g.pop("db", None)
+    if db is not None:
+        db.close()
+
+
+print("PostgreSQL ready")
+
+
 
 @app.route("/users", methods=["GET"])
 def get_users():
     db = get_db()
-    cursor = db.cursor(dictionary=True)
+    cursor = db.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+
     cursor.execute("SELECT uid, post, password FROM users ORDER BY post")
     data = cursor.fetchall()
+
     cursor.close()
-    db.close()
     return jsonify(data)
+
 
 @app.route("/login", methods=["POST"])
 def login():
     data = request.get_json()
-
     uid = data.get("uid")
     password = data.get("password")
 
     db = get_db()
-    cursor = db.cursor(dictionary=True)
+    cursor = db.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
 
     cursor.execute(
         "SELECT * FROM users WHERE uid=%s AND password=%s",
@@ -42,19 +55,19 @@ def login():
     user = cursor.fetchone()
 
     cursor.close()
-    db.close()
 
     if user:
         return jsonify({"status": "success"})
     else:
         return jsonify({"status": "fail"})
 
+
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
 if __name__ == "__main__":
     app.run(debug=True)
-
 
 
